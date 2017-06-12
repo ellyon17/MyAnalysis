11434c050c11e4540a463804efb5638172714444
Presani <PresaniE@science.regn.net>
2017-06-12 13:07:31 -0400

    first commit

---
+++ src/analysis.js
@@ -0,0 +1,18 @@
+d3.text("../data/CTOexport.csv", function(data) {
+    input = d3.csvParseRows(data).map(function(row) {
+        return row.map(function(value, index) {
+			if(index == 1 || index == 2 || index == 4){
+				return value;
+			} else {
+				return +value;
+			}
+        });
+    });
+	    // do stuff here
+	
+	console.log("il mio csv:");	
+	console.log(data);	
+		
+		
+});
+
