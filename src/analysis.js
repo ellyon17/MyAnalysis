8e67d14640f55ff96239871a6c8db3b3c2b23124
Presani <PresaniE@science.regn.net>
2017-06-13 15:59:52 -0400

    some updates

--- src/analysis.js
+++ src/analysis.js
@@ -1,18 +1,49 @@
-d3.text("../data/CTOexport.csv", function(data) {
-    input = d3.csvParseRows(data).map(function(row) {
-        return row.map(function(value, index) {
-			if(index == 1 || index == 2 || index == 4){
-				return value;
-			} else {
-				return +value;
-			}
-        });
-    });
-	    // do stuff here
-	
-	console.log("il mio csv:");	
-	console.log(input[1]);	
-		
-		
-});
 
+// find last year (to calculate CS)
+function findCSyear(yr) {
+	var d = new Date();
+	var n = d.getFullYear() - 1;
+	var CSyr = n.toString();
+    return yr == CSyr;
+}
+
+// transform an array of strings into Int
+function toIntArray(arr) {
+    for (var i = 0; i < arr.length; i++) { 
+        arr[i] = +arr[i]; 
+    } 
+    return arr;
+}
+
+d3.csv('/data/CTOexport.csv', function(error, data) {
+//d3.csv('/data/CTOTest.csv', function(error, data) {
+	
+/*
+This is to decide for which year we should calculate the CS, based on the data available
+Based on the curent calendar year, we assume that the most recent complete year it's last year. 
+If that's not present we take the most recent year in the header	
+*/
+	
+	var headers = d3.keys(data[0]);
+	console.log(headers);
+	var CSyr = 0;
+	
+	var lastYr = headers.find(findCSyear);
+ 
+	if(lastYr){
+		CSyr = parseInt(lastYr);
+	}else{
+		var headers_int = toIntArray(headers);
+		CSYr = d3.max(headers_int);
+	}
+	
+	console.log("calculating CiteScore for "+CSyr);
+	
+/*
+Make sure that there are articles from the 3 years antecedent the CiteScore year
+*/
+	
+	
+    
+})
+ 
