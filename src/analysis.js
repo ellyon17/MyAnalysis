6c387eab1762497b9a422c1499043c150226041d
Presani <PresaniE@science.regn.net>
2017-06-14 16:25:20 -0400

    some updates

--- src/analysis.js
+++ src/analysis.js
@@ -18,14 +18,26 @@ function toIntArray(arr) {
 d3.csv('/data/CTOexport.csv', function(error, data) {
 //d3.csv('/data/CTOTest.csv', function(error, data) {
 	
+	var headers = d3.keys(data[0]);
+	data.forEach(function(d){ 
+		d["Publication Year"] = +d["Publication Year"]; 
+		d.ISSN = +d.ISSN;
+		d.Issue = +d.Issue;
+		d.Volume = +d.Volume;
+		d.subtotal = +d.subtotal;
+		d.total = +d.total;
+	});
+	
+	console.log(data[5]);
+    
 /*
 This is to decide for which year we should calculate the CS, based on the data available
 Based on the curent calendar year, we assume that the most recent complete year it's last year. 
 If that's not present we take the most recent year in the header	
 */
 	
-	var headers = d3.keys(data[0]);
-	console.log(headers);
+
+	//console.log(headers);
 	var CSyr = 0;
 	
 	var lastYr = headers.find(findCSyear);
@@ -42,8 +54,22 @@ If that's not present we take the most recent year in the header
 /*
 Make sure that there are articles from the 3 years antecedent the CiteScore year
 */
-	
-	
-    
+	var ctrl_1 = 0;
+	var ctrl_2 = 0;
+
+	data.forEach(function(d){ 
+
+	if(d["Publication Year"] == (CSyr-1)){ctrl_1 = 1;};
+	if(d["Publication Year"] == (CSyr-2)){ctrl_2 = 1;};	
+	})
+	if (ctrl_1+ctrl_2 == 2){ 
+		console.log("We have all the data to calculate the IF!");
+	} else {
+		console.log(" You're missing some data to calculate the IF, remember to add citations from 2 years prior to "+CSyr);
+	}
+
+
+
+
 })
  
